57398cadb5869b5f6396865dc2b0eb1bec3f56e9
ReahKim <reah@supertree.co>
2021-06-25 17:12:40 +0900

    feat: 두더지 잡기 게임

---
+++ src/components/Game/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StartScreen from '../StartScreen'
+
+const Game = () => {
+  return (
+    <>
+      <StartScreen />
+    </>
+  )
+}
+
+export default Game
---
+++ src/components/Mole/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Mole = () => {
+  return <div></div>
+}
+
+export default Mole
